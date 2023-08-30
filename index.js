506eba08efea6b667e43953fb0543035e36f267a
Kenneth <kvmoore097@gmail.com>
2023-08-29 19:12:07 -0500

    phase-1 looping code along

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+
+
+const names = [];
+
+function writeCards(names, event = "birthday") {
+    
+    let messages= [];
+
+    for (let i = 0; i < names.length; i++) {
+
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+      
+    }
+   return messages;
+}
+console.log(writeCards(["john", "george", "sam"], "graduation"));
+
+
+function countDown(Integer){
+
+Integer = 10
+while (Integer >= 0){
+  console.log(Integer--);
+}
+
+}
